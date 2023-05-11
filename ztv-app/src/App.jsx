47d1adbc37665033a9edfb9156c5b68680ce652a
unknown <jorge.orozco@cetys.edu.mx>
2023-05-11 13:47:25 -0700

    ztv-app init and Home component

---
+++ ztv-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Home } from '../src/components/Home'
+
+function App () {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Home />
+
+    </>
+  )
+}
+
+export default App
