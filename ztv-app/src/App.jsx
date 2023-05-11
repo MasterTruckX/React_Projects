8c903b225f1df44635c8ec0bbe00c76c04308b6d
unknown <jorge.orozco@cetys.edu.mx>
2023-05-11 15:17:35 -0700

    BrowserRouter added

--- ztv-app/src/App.jsx
+++ ztv-app/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Home from './components/Home'
+import { BrowserRouter } from 'react-router-dom'
 
 function App () {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
 
     </>
   )
