79430fa8cfebf49f9b56ad38d72c6a87dc3403d0
unknown <jorge.orozco@cetys.edu.mx>
2023-05-17 11:51:44 -0700

    Home added to pages file

--- ztv-app/src/routes/index.jsx
+++ ztv-app/src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from '../components/Home'
+import Home from '../pages/Home'
 import ZerieDetail from '../components/ZerieDetail'
 
 const RoutesIndex = () => {
