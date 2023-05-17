77c8efc387ec4098dc9d4cbbb6e0d34958f696b4
unknown <jorge.orozco@cetys.edu.mx>
2023-05-17 11:49:23 -0700

    Cast display

---
+++ ztv-app/src/components/Cast.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+
+const Cast = ({ id }) => {
+  const [cast, setCast] = useState([])
+
+  const sendCast = () => {
+    fetch(`https://api.tvmaze.com/shows/${id}/cast`)
+      .then(response => response.json())
+      .then(cast => setCast(cast))
+      .catch(error => console.error(error))
+  }
+
+  useEffect(() => {
+    sendCast()
+  }, [])
+
+  // useEffect(() => {
+  //   fetch(`https://api.tvmaze.com/shows/${id}/cast`)
+  //     .then(response => response.json())
+  //     .then(data => setCast(data.results))
+  //     .catch(error => console.error(error))
+  // })
+  console.log(cast)
+  return (
+    <div className='col-md-12'>
+      <h3>Cast</h3>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Actor</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cast.map(person => (
+            <tr key={person.person.url}>
+              <td>{person.person.name}</td>
+              <td>{person.character.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Cast
--- ztv-app/src/components/ZerieDetail.jsx
+++ ztv-app/src/components/ZerieDetail.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import parse from 'html-react-parser'
+import Cast from './Cast'
 
 const ZerieDetail = () => {
   const { id } = useParams()
@@ -16,6 +17,12 @@ const ZerieDetail = () => {
   if (!zerie) {
     return <div>Cargando...</div>
   }
+  // {"person":{"id":1,"url":"https://www.tvmaze.com/people/1/mike-vogel","name":"Mike Vogel","country":{"name":"United States","code":"US","timezone":"America/New_York"},
+  //   "birthday":"1979-07-17","deathday":null,"gender":"Male","image":{"medium":"https://static.tvmaze.com/uploads/images/medium_portrait/0/1815.jpg",
+  // "original":"https://static.tvmaze.com/uploads/images/original_untouched/0/1815.jpg"},"updated":1672996558,"_links":{"self":{"href":"https://api.tvmaze.com/people/1"}}},
+  // "character":{"id":1,"url":"https://www.tvmaze.com/characters/1/under-the-dome-dale-barbie-barbara",
+  // "name":"Dale \"Barbie\" Barbara","image":{"medium":"https://static.tvmaze.com/uploads/images/medium_portrait/0/3.jpg",
+  //  "original":"https://static.tvmaze.com/uploads/images/original_untouched/0/3.jpg"},"_links":{"self":{"href":"https://api.tvmaze.com/characters/1"}}},"self":false,"voice":false},
 
   return (
     <div className='container mt-3'>
@@ -26,7 +33,7 @@ const ZerieDetail = () => {
         <div className='card-body'>
           <div className='row'>
             <div className='col-md-4'>
-              <img src={`${zerie.image.medium}`} alt={zerie.name} className='img-fluid' />
+              <img src={`${zerie?.image?.medium}`} alt={zerie.name} className='img-fluid' />
             </div>
             <div className='col-md-8'>
               <h2>Synopsis</h2>
@@ -34,6 +41,13 @@ const ZerieDetail = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className='episodes'>
+        <h3>Episodes</h3>
+        <table />
+      </div>
+      <div className='cast'>
+        <Cast id={id} />
       </div>
     </div>
   )
