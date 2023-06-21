6635604d67433a6fd78aed176c5443ad9e55557b
unknown <jorge.orozco@cetys.edu.mx>
2023-06-21 01:06:30 -0700

    Dashboard page added

--- JayShop/src/pages/Dashboard.jsx
+++ JayShop/src/pages/Dashboard.jsx
@@ -1,6 +1,38 @@
+import { useAuthContext } from '@/hooks/useAuth'
+import { useState, useEffect } from 'react'
+import { getSingleUserService } from '@/services/userService'
 const Dashboard = () => {
+  const { userPayload } = useAuthContext()
+  const [userData, setUserData] = useState({})
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getSingleUserService(userPayload.id)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrio un error:', error.message)
+      }
+    }
+    fetchUserData()
+  }, [userPayload.id])
   return (
-    <div>Dashboard</div>
+    <>
+      <h1>Dashboard</h1>
+      {
+        userData?.first_name && <p>First Name: {userData.first_name}</p>
+      }
+      {
+        userData?.last_name && <p>Last Name: {userData.last_name}</p>
+      }
+      {
+        userData?.gender && <p>Gender: {userData.gender}</p>
+      }
+      {
+        userData?.email && <p>Email: {userData.email}</p>
+      }
+    </>
   )
 }
 
